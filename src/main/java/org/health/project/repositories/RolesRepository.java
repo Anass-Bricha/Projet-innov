306a9306d9b0774b73fdf94b9b6bb2f9ce57361e
Anass Bricha <anassebricha01@gmail.com>
2022-05-29 00:04:09 +0100

    FINAL COMMIT BACK-END

--- src/main/java/org/health/project/repositories/RolesRepository.java
+++
@@ -1,7 +0,0 @@
-package org.health.project.repositories;
-
-import org.health.project.entites.Roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolesRepository extends JpaRepository<Roles,Long> {
-}
